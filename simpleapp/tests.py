0a6f5202371e25c8822b0e6ac3c17146df0cc91d
ali <bad199614@gmail.com>
2019-07-29 13:13:57 +0500

    2.1

--- simpleapp/tests.py
+++ simpleapp/tests.py
@@ -10,7 +10,8 @@ class SimpleModelTestCase(TestCase):
     def test_create_get_object(self):
         sobj = SimpleModel.objects.create(name='Software13432434',description='this is description for software13432434',
         driver='ORACLE',host='193.33.23.33',port=80,username='user3423424',password="pass2343434343")
-        print(sobj.name, sobj.driver, sobj.host, sobj.port)
+        s_details: list = [sobj.name,sobj.driver,sobj.host,sobj.port]
+        print(s_details)
         obj_id: str = str(sobj.id)
         response = self.client.get('/api/v1/'+ obj_id + '/')
         self.assertEquals(response.status_code,200)
